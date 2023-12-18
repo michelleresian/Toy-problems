bd8ef4cc966a81137ff510f21e9ae3eee63fd710
Michelle Resian <you@example.com>
2023-12-18 13:43:40 +0300

    netsalary

--- toyproblem/netsalarycalculator.js
+++ toyproblem/netsalarycalculator.js
@@ -1,80 +1,70 @@
 function calculateNetSalary(basicSalary, benefits) {
-    const taxableAmount = basicSalary + benefits; //this is the total income before taxation and deduction.
-  
-    const taxRates = [
-      //the rate is in percentage. for example 10% = 10/100 = 0.1
-      { from: 0, to: 24000, rate: 0.1 },
-      { from: 24001, to: 32333, rate: 0.25 }, // 25/100
-      { from: 32334, to: 500000, rate: 0.3 },
-      { from: 500001, to: 800000, rate: 0.325 },
-      { from: 800001, to: Infinity, rate: 0.35 },
-    ];
-    let tax = 0; // Initialize variable for tax
-    for (const taxRate of taxRates) {
-      if (taxableAmount >= taxRate.from && taxableAmount <= taxRate.to) //calculates tax based on the range 
-       {
-        tax += (taxableAmount - taxRate.from) * taxRate.rate;
-      } 
-      else if (taxableAmount > taxRates.to) //calculates tax for income above range.
-       {
-        tax += (taxRate.to - taxRate.from) * taxRate.rate;
-      }
+  const taxableAmount = basicSalary + benefits;
+
+  const taxRates = [
+    { from: 0, to: 24000, rate: 0.1 },
+    { from: 24001, to: 32333, rate: 0.25 },
+    { from: 32334, to: 500000, rate: 0.3 },
+    { from: 500001, to: 800000, rate: 0.325 },
+    { from: 800001, to: Infinity, rate: 0.35 },
+  ];
+
+  let tax = 0;
+
+  for (const taxRate of taxRates) {
+    if (taxableAmount >= taxRate.from && taxableAmount <= taxRate.to) {
+      tax += (taxableAmount - taxRate.from) * taxRate.rate;
+      break; // Exit the loop once the tax is calculated
+    } else if (taxableAmount > taxRate.to) {
+      tax += (taxRate.to - taxRate.from) * taxRate.rate;
     }
-  
-    const nhifRates = [
-      { from: 0, to: 5999, deduction: 150 },
-      { from: 6000, to: 7999, deduction: 300 },
-      { from: 8000, to: 11999, deduction: 400 },
-      { from: 12000, to: 14999, deduction: 500 },
-      { from: 15000, to: 19999, deduction: 600 },
-      { from: 20000, to: 24999, deduction: 750 },
-      { from: 25000, to: 29999, deduction: 850 },
-      { from: 30000, to: 34999, deduction: 900 },
-      { from: 35000, to: 39999, deduction: 950 },
-      { from: 40000, to: 44999, deduction: 1000 },
-      { from: 45000, to: 49999, deduction: 1100 },
-      { from: 50000, to: 59999, deduction: 1200 },
-      { from: 60000, to: 69999, deduction: 1300 },
-      { from: 70000, to: 79999, deduction: 1400 },
-      { from: 80000, to: 89999, deduction: 1500 },
-      { from: 90000, to: 99999, deduction: 1600 },
-      { from: 100000, to: Infinity, deduction: 1700 },
-    ];
-  
-    let nhifdeduduction=0; //initialize nhif deduction
-  
-    //calculates nhif deduction based on income
-    for(const nhifRate of nhifRates){
-      if(taxableAmount>=nhifRate.from && taxableAmount <= nhifRate.to){
-        nhifdeduduction= taxableAmount-(taxableAmount - nhifRate.deduction)
-      }
+  }
+
+  const nhifRates = [
+    { from: 0, to: 5999, deduction: 150 },
+    { from: 6000, to: 7999, deduction: 300 },
+    { from: 8000, to: 11999, deduction: 400 },
+    { from: 12000, to: 14999, deduction: 500 },
+    { from: 15000, to: 19999, deduction: 600 },
+    { from: 20000, to: 24999, deduction: 750 },
+    { from: 25000, to: 29999, deduction: 850 },
+    { from: 30000, to: 34999, deduction: 900 },
+    { from: 35000, to: 39999, deduction: 950 },
+    { from: 40000, to: 44999, deduction: 1000 },
+    { from: 45000, to: 49999, deduction: 1100 },
+    { from: 50000, to: 59999, deduction: 1200 },
+    { from: 60000, to: 69999, deduction: 1300 },
+    { from: 70000, to: 79999, deduction: 1400 },
+    { from: 80000, to: 89999, deduction: 1500 },
+    { from: 90000, to: 99999, deduction: 1600 },
+    { from: 100000, to: Infinity, deduction: 1700 },
+  ];
+
+  let nhifdeduction = 0;
+
+  for (const nhifRate of nhifRates) {
+    if (taxableAmount >= nhifRate.from && taxableAmount <= nhifRate.to) {
+      nhifdeduction = nhifRate.deduction;
+      break; // Exit the loop once the deduction is found
     }
-  
-    let nssfDeduction=0;
-    const nssfRate=0.06 //rate is 6% (6/100) = 0.06
-  nssfDeduction=taxableAmount*nssfRate //calculates nssf deductions
-  
-  const netSalary = taxableAmount-(tax+nhifdeduduction+nssfDeduction) //calculate net salary by subtracting the tax and all the other deductions
-  
-  const salary={ // object that stores all the salary details
+  }
+
+  const nssfDeduction = taxableAmount * 0.06;
+
+  const netSalary = taxableAmount - (tax + nhifdeduction + nssfDeduction);
+
+  const salary = {
     taxableAmount,
     tax,
-    nhifdeduduction,
+    nhifdeduction,
     nssfDeduction,
     netSalary,
   };
-  
+
   return salary;
-  
-  }
-  
-  const basicSalary=50000;
-  const benefits=20000;
-  const result= calculateNetSalary(basicSalary, benefits);
-  console.log(result);
-  /**{taxableAmount: 70000, 
-   * tax: 11299.8, 
-   * nhifdeduduction: 1400, 
-   * nssfDeduction: 4200, 
-   * netSalary: 53100.2 **/
-  
+}
+
+const basicSalary = 50000;
+const benefits = 20000;
+const result = calculateNetSalary(basicSalary, benefits);
+console.log(result);
--- toyproblem/speedDetector.js
+++ toyproblem/speedDetector.js
@@ -1,22 +1,20 @@
-function speedDetector(speed) {
-    const speedLimit = 70; // Speed limit
-    let demeritPoints = 0;
-  
-    if (speed <= speedLimit) {
-      return "Ok"; // No demerit points if speed is within the limit
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerDemeritPoint = 5;
+  const maxDemeritPoints = 12;
+
+  if (speed <= speedLimit) {
+    console.log("Ok");
+  } else {
+    const excessSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+
+    if (demeritPoints >= maxDemeritPoints) {
+      console.log("License suspended");
+    } else {
+      console.log("Points: " + demeritPoints);
     }
-  
-    const speedAbove = speed - speedLimit; // Calculate how much the speed is above the limit
-    demeritPoints = Math.floor(speedAbove / 5); // Calculate demerit points
-  
-    if (demeritPoints > 12) {
-      return "License suspended"; // Check for license suspension
-    }
-  
-    return `Points: ${demeritPoints}`;
   }
-  
-  console.log(speedDetector(80)); // Output: "Points: 2"
-  console.log(speedDetector(65)); // Output: "Ok"
-  console.log(speedDetector(95)); // Output: "Points: 5"
-  console.log(speedDetector(130)); // Output: "License suspended"
+}
+
+console.log (speed);
